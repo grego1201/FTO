6319bd6666065b95cb948c6933c2354115188d3b
grego1201 <gregoriopatinoesteo@gmail.com>
2021-11-03 18:58:17 +0100

    Add rubocop

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Competition < ApplicationRecord
   validates_presence_of :name, :date, :gender
-  validates :gender, inclusion: { in: %w(male female other mix), message: 'Gender not valid' }
-  validates :category, inclusion: { in: %w(u11 u13 u15 u17 u20 u23 abs vet),
+  validates :gender, inclusion: { in: %w[male female other mix], message: 'Gender not valid' }
+  validates :category, inclusion: { in: %w[u11 u13 u15 u17 u20 u23 abs vet],
                                     message: 'category not valid' }, allow_nil: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,19 +12,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_213405) do
-
+ActiveRecord::Schema.define(version: 20_211_102_213_405) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "competitions", force: :cascade do |t|
-    t.string "name"
-    t.date "date"
-    t.string "location"
-    t.string "gender"
-    t.string "category"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'competitions', force: :cascade do |t|
+    t.string 'name'
+    t.date 'date'
+    t.string 'location'
+    t.string 'gender'
+    t.string 'category'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
