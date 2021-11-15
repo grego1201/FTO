20158ed94da0ad191d4d911719f8f8c9b5d9b631
grego1201 <gregoriopatinoesteo@gmail.com>
2021-11-15 21:54:13 +0100

    Add basic competition CRUD

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Competition < ApplicationRecord
+  CATEGORIES = %w[u11 u13 u15 u17 u20 u23 abs vet]
+  GENDERS = %w[male female other mix]
+
   validates_presence_of :name, :date, :gender
-  validates :gender, inclusion: { in: %w[male female other mix], message: 'Gender not valid' }
-  validates :category, inclusion: { in: %w[u11 u13 u15 u17 u20 u23 abs vet],
-                                    message: 'category not valid' }, allow_nil: true
+  validates :gender, inclusion: { in: GENDERS, message: 'Gender not valid' }
+  validates :category, inclusion: { in: CATEGORIES, message: 'category not valid' }, allow_nil: true
 end
